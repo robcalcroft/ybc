b08cdd6f11550a35ee69a05fe285824f4f417099
robcalcroft <rob@calcroft.com>
2015-03-22 16:31:20 +0000

    Added linkedin updates to the site on the front page and the testimonials page

--- src/js/main.js
+++ src/js/main.js
@@ -19,5 +19,29 @@ $(document).ready(function() {
 	// Show hide nav menu
 	$('.nav-show-mobile').click(function() {
 		$('#nav').slideToggle();
-	})
-})
+	});
+
+});
+
+function updateRunner() {
+	if($('#updates').length) {
+		IN.API.Raw('companies/9333069/updates?event-type=status-update&format=json').method('GET').result(function(d){showUpdates(d, true, $('#updates'))});
+	}
+	if($('#archived-updates').length) {
+		IN.API.Raw('companies/9333069/updates?event-type=status-update&format=json').method('GET').result(function(d){showUpdates(d, false, $('#archived-updates'))});
+	}
+}
+
+function showUpdates(updates, first, el) {
+	var updatesLen = updates.values.length;
+	if (updatesLen === 1 && !first) { el.append('No archived updates') };
+	while(updatesLen--) {
+		if(!first) { continue; }
+		var content = updates.values[updatesLen].updateContent.companyStatusUpdate.share.comment,
+			likes = updates.values[updatesLen].likes._total,
+			d = new Date(updates.values[updatesLen].timestamp); 
+		d = d.getDay()+'/'+(d.getMonth()+1)+'/'+d.getFullYear();;
+		el.append('<div class="updates">' + content + '<div style="color:gray;">'+d+' - '+likes+' likes</div></div>');
+		if(first) { break; }
+	}
+}
